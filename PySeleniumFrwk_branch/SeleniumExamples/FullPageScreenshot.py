e9762c6953b0d571229b9b7939f057c5102535f7
Pammugs <87618109+Pammugs@users.noreply.github.com>
2021-09-25 23:27:46 +0530

    formatting in the page

--- PySeleniumFrwk_branch/SeleniumExamples/FullPageScreenshot.py
+++ PySeleniumFrwk_branch/SeleniumExamples/FullPageScreenshot.py
@@ -1,30 +1,18 @@
-import time
-
 from selenium import webdriver
-
 from webdriver_manager.chrome import ChromeDriverManager
-
 from selenium.webdriver.chrome.options import Options
 
 
 chrome_options = Options()
-
-
-#chrome_options = webdriver.ChromeOptions()
-
-
+# chrome_options = webdriver.ChromeOptions()
 chrome_options.headless = True
-
 driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(),options=chrome_options)
-
-driver.get("http://www.way2automation.com/")
+# driver.get("http://www.way2automation.com/")
+# driver.get("https://www.udemy.com/")
 driver.maximize_window()
 driver.implicitly_wait(1)
-
-
-S = lambda X: driver.execute_script('return document.body.parentNode.scroll' +X)
-driver.set_window_size(S('Width'),S('Height'))
-
-driver.find_element_by_tag_name('body').screenshot('./screenshot/fullpage.jpg')
+scroll = lambda exp: driver.execute_script('return document.body.parentNode.scroll' + exp)
+driver.set_window_size(scroll('Width'), scroll('Height'))
+driver.find_element_by_tag_name('body').screenshot('./screenshot/fullpage3.jpg')
 
 
