bfd8c551e8aeb0881e227d2a35e75fecb958ce05
Pammugs <87618109+Pammugs@users.noreply.github.com>
2021-10-06 14:22:12 +0530

    changes

--- PySeleniumFrwk_branch/SeleniumExamples/FullPageScreenshot.py
+++ PySeleniumFrwk_branch/SeleniumExamples/FullPageScreenshot.py
@@ -6,13 +6,13 @@ from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 # chrome_options = webdriver.ChromeOptions()
 chrome_options.headless = True
-driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(),options=chrome_options)
+driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=chrome_options)
 # driver.get("http://www.way2automation.com/")
-# driver.get("https://www.udemy.com/")
+driver.get("https://www.udemy.com/")
 driver.maximize_window()
 driver.implicitly_wait(1)
-scroll = lambda exp: driver.execute_script('return document.body.parentNode.scroll' + exp)
-driver.set_window_size(scroll('Width'), scroll('Height'))
+area = lambda exp: driver.execute_script('return document.body.parentNode.scroll' + exp)
+driver.set_window_size(area('Width'), area('Height'))
 driver.find_element_by_tag_name('body').screenshot('./screenshot/fullpage3.jpg')
 
 
